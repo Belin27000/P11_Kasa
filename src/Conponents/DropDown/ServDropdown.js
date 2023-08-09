b093eae2d2ce139dc962c145e6dfbf49f4d512a7
Belin27000 <y.lecerf@protonmail.com>
2023-08-09 22:34:19 +0200

    Feat: AboutPage Service dropdown added

--- src/Conponents/DropDown/ServDropdown.js
+++ src/Conponents/DropDown/ServDropdown.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaAngleDown } from "react-icons/fa";
+
 
 const ServDropdown = () => {
+
+    const [hide, setHide] = useState()
+    const [rotate, setRotate] = useState()
+
+    const hideDes = () => {
+        setHide(!hide)
+        setRotate(!rotate)
+    }
     return (
         <div className='ServDropdown'>
-            Service OK!
+            <div className='HeaderDetails'>
+                <div className='Name'>Service</div>
+                <FaAngleDown className={rotate ? 'Arrow rotate' : 'Arrow'} onClick={hideDes} />
+
+            </div>
+            <div className={hide ? 'Detail Detail-hidden' : 'Detail'}>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</div>
         </div>
     );
 };
